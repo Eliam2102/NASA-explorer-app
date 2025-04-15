691fb13c83ffd5f286920bdef4e3ac776482a9a4
Eliam2102 <157552853+Eliam2102@users.noreply.github.com>
2025-04-15 01:00:01 -0600

    Feat: navegación principal drawer
    Se sube avance de la navegación con los ítems del drawer, ya establecidos con un poco de color y navegación, restando complementar con navegación anidad (nested navigation).

---
+++ src/navigation/DrawerNavigation.tsx
@@ -0,0 +1,120 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import React from "react";
+// Importar tema
+import { nasaLightTheme } from "../core/themeLight";
+// Módulo de iconos de FontAwesome5
+import { FontAwesome5 } from '@expo/vector-icons';
+import HomeScreen from "../presentation/screens/HomeScreen";
+import AstronomyScreen from "../presentation/screens/AstronomyScreen";
+import MediaScreen from "../presentation/screens/MediaScreen";
+import SettingsScreen from '../presentation/screens/SettingsScreen';
+import NewScreen from "../presentation/screens/NewsScreen";
+import PlanetsScreen from "../presentation/screens/PlanetsScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import MoreExlporerScreen from "../presentation/screens/MoreExplorerScreen";
+
+// Crea una instancia del drawer navigator
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigation() {
+  return (
+    <Drawer.Navigator 
+      initialRouteName="index"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: nasaLightTheme.primary,  // Fondo del encabezado
+        },
+        headerTintColor: nasaLightTheme.onPrimary,  // Color del texto en el encabezado
+        drawerStyle: {
+          backgroundColor: nasaLightTheme.background,  // Fondo del Drawer
+        },
+        drawerActiveTintColor: nasaLightTheme.primary,  // Color de los ítems activos en el Drawer
+        drawerInactiveTintColor: nasaLightTheme.textSecondary,  // Color de los ítems inactivos en el Drawer
+      }}
+    >
+      {/* Región para ítem de Inicio */}
+      <Drawer.Screen
+        name="index"
+        component={HomeScreen}
+        options={{
+          title: 'Inicio',
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome5 name="space-shuttle" color={nasaLightTheme.secondary} size={size} />
+          ),
+        }}
+      />
+
+      {/* Región para ítem de Astronomía */}
+      <Drawer.Screen
+        name="astronomy"
+        component={AstronomyScreen}
+        options={{
+          title: 'Astronomía',
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome5 name="meteor" color={nasaLightTheme.secondary} size={size} />
+          ),
+        }}
+      />
+
+      {/* Región para el ítem de Multimedia */}
+      <Drawer.Screen
+        name="media"
+        component={MediaScreen}
+        options={{
+          title: 'Multimedia',
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome5 name="satellite-dish" color={nasaLightTheme.secondary} size={size} />
+          ),
+        }}
+      />
+
+      {/* Región para ítem de Noticias */}
+      <Drawer.Screen
+        name="news"
+        component={NewScreen}
+        options={{
+          title: 'Noticias',
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome5 name="satellite" color={nasaLightTheme.secondary} size={size} />
+          ),
+        }}
+      />
+
+      {/* Región para ítem de Planetas */}
+      <Drawer.Screen
+        name="planets"
+        component={PlanetsScreen}
+        options={{
+          title: 'Planetas',
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome5 name="globe" color={nasaLightTheme.secondary} size={size} />
+          ),
+        }}
+      />
+
+      {/* Región para ítem de Explorar más */}
+      <Drawer.Screen
+        name="exploremore"
+        component={MoreExlporerScreen}
+        options={{
+          title: 'Explorar más',
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome5 name="rocket" color={nasaLightTheme.secondary} size={size} />
+          ),
+        }}
+      />
+
+      {/* Región para ítem de Configuración */}
+      <Drawer.Screen
+        name="settings"
+        component={SettingsScreen}
+        options={{
+          title: 'Configuración',
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome5 name="flask" color={nasaLightTheme.secondary} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
