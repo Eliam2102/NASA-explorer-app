4b502808c6d2d2b25e9bfcfbf1a8c21b29731878
Eliam2102 <157552853+Eliam2102@users.noreply.github.com>
2025-04-17 09:06:04 -0600

    Fix: Se corrige el splash image
    Se corrige el splash image para manejarlo más nativo de cada una de las plataformas que se pudieran usar.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
 // App.tsx
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import SplashScreen from './src/components/splashScreen';
-import { useState } from 'react';
-
-
+import { StatusBar } from 'react-native';2
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigation from './src/navigation/DrawerNavigation';
 
 export default function App() {
-    const [showSplash, setShowSplash] = useState(true);
-  
-    if (showSplash) {
-      return <SplashScreen onFinish={() => setShowSplash(false)} />;
-    }
-  
+
     return (
       <NavigationContainer>
         <DrawerNavigation />
